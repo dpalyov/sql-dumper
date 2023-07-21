bed3a4573368be92eb54cd607718e124f84e4bde
Dimitar Palyov <dimitar.palyov@amusnet.com>
2023-07-21 16:15:51 +0300

    improving install script

--- start.sh
+++ start.sh
@@ -1,7 +1,20 @@
-#! /bin/bash
+#!/bin/bash
+
+echo Would you like to install the app globally? [y/n]
+
+read response 
+
+if [[ $response == 'y' ]]; then
+	echo Installing in "$HOME/.local/bin" directory
+	cp ./dist/main $HOME/.local/bin/sqldumper;
+	echo Run 'sqldumper' to start.
+	exit;
+fi
+
+echo Installing locally...
 
 pip install -r requirements.txt
-
 cp .env.example .env
-
 python3 ./main.py
+
+echo Run 'python3 ./main.py' to start
